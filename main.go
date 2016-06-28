a7ca5144ebfeba50b735c1c817019586005d4d8a
Huy Giang <gnhuy91@gmail.com>
2016-06-28 10:35:01 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Message struct {
+	Text string `json:"msg"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	s := fmt.Sprintf("Welcome!")
+	j, _ := json.Marshal(&Message{s})
+	w.Write(j)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	m := Message{"go API, build v0.0.001.992."}
+	j, _ := json.Marshal(&m)
+	w.Write(j)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/about", about)
+	http.ListenAndServe(":8080", nil)
+}
